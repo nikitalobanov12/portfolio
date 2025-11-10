8f87136109b3c84aee7374fd630b84c242380c8a
Nikita Lobanov <nikitaalobanovv@gmail.com>
2025-11-09 19:44:42 -0800

    update to content

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,7 @@ export const intro = {
   name: "Nikita Lobanov",
   // title: "Full Stack Developer",
   description:
-    "I'm a 21 year old full stack developer from Vancouver, BC. \n I'm very curious about things that are interesting :)",
+    "I'm a 21 year old full stack developer from Vancouver, BC. \n I'm very curious about things that are interesting :)\nMy projects are either me trying to solve a problem in my day to day life or for learning concepts",
 };
 
 export const projects = [
@@ -18,7 +18,7 @@ export const projects = [
     url: "https://github.com/nikitalobanov12/writeshare",
     liveUrl: "https://writeshare.nikitalobanov.com",
     description:
-      "Real-time collaborative markdown editor, For me this was a way to learn about Websockets and real time interactive features, as well as learning more about dockerizing applications and hosting on AWS",
+      "Real-time collaborative markdown editor, For me this was a way to learn about Websockets, real time interactive features, containerization, and hosting on AWS",
   },
   {
     title: "Dayflow",
@@ -34,6 +34,13 @@ export const projects = [
     description:
       "Private community photo sharing app, like google drive combined with instagram. For me this was a good learning experience about the fundamentals of a web app & I learned a lot from building this",
   },
+  {
+    title: "H2L Design Studio",
+    url: "https://github.com/nikitalobanov12/H2L-Design-Studio",
+    liveUrl: "https://h2ldesignstudio.com",
+    description:
+      "This is a portfolio website I did for a local freelancer, one of my first web dev projects",
+  },
 ];
 
 export const experience = [
@@ -42,7 +49,7 @@ export const experience = [
     company: "Seaspan Corp",
     url: "https://www.seaspancorp.com",
     description:
-      "Worked on internal tooling for company that manages cargo ships, more detail on LinkedIn if you care",
+      "Worked on internal tooling for company that manages cargo ships, more info on LinkedIn if interested",
   },
   // Add more experiences as needed
 ];
